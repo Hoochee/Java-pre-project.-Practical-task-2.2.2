b7d2247c04fa654915f2c661b9540e9e0bb4ef71
Hoochee <jok777@yandex.ru>
2022-01-29 13:43:57 +0300

    Java-pre-project.-Practical-task-2.2.2 - refactoring

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,5 +1,6 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,10 +15,17 @@ import java.util.Optional;
 @RequestMapping()
 public class CarController {
 
+    private final CarService service;
+
+    @Autowired
+    public CarController(CarService service) {
+        this.service = service;
+    }
+
     @GetMapping("/cars")
-    public String index(@RequestParam(value = "count", required = false) Optional<Integer> count, Model model){
-        CarService service = new CarServiceImpl();
-        model.addAttribute("cars",service.index(count.orElse(0)));
+    public String index(@RequestParam(value = "count", required = false) Optional<Integer> count, Model model) {
+
+        model.addAttribute("cars", service.index(count.orElse(0)));
         return "cars";
     }
 }
